496b3a7f97638ccd2cbe3288413af67099a2ed2e
Alexis <alexie0512@163.com>
2020-05-22 23:20:27 +0800

    JAVA 3_add update pom and tests

--- src/test/java/TestJUnit5.java
+++ src/test/java/TestJUnit5.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestJUnit5 {
     @Test
     void testcaseDemo1(){
-        assertEquals(1,2);
+        assertEquals(1,1);
 
     }
 
